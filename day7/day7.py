efd8d08be02f71b944fbe4e396253c9aa6f69fb7
Emanuel Ferreira <ejvlf@outlook.pt>
2021-12-09 13:17:35 +0000

    day7 part2

--- day7/day7.py
+++ day7/day7.py
@@ -2,20 +2,47 @@ import sys
 sys.path.append("/home/ejvlf/personal/adventofcode2021")
 print(sys.path)
 
-from statistics import median
+from statistics import median, mean
 from common.text_manipulations import TextParser
+
+def fuel_spent (reference_value : float, distance_list : list) -> float:
+
+    fuel_spent = sum(map(lambda distance : abs(distance - reference_value), distance_list))
+
+    return fuel_spent
+    
+def fuel_spent_part_2(start: int, end: int):
+
+    return sum(range(abs(end - start)+1))
 
 def run():
 
     #source file loading
     crab_fuel : list = TextParser("day7.txt").load_file_as_ints()
-    
-    distance_median = median(crab_fuel)
-    fuel_spent = sum(map(lambda distance : abs(distance - distance_median), crab_fuel))
+        
 
     #Part 1
-    print(f"Part 1 score is {fuel_spent}")
+    distance_median = median(crab_fuel)
+    part1 = fuel_spent(distance_median, crab_fuel)
+    print(f"Part 1 score is {part1}")
 
+    #Part 2
+    range_start = min(crab_fuel)
+    range_end   = max(crab_fuel)
+    best_point = 0
+    best_fuel  = 100000000000000000
+
+    for distance in range(range_start,range_end):
+
+        def cost(start : int, end : int):
+            return sum(range(abs(end - start)+1))
+
+        current = sum([cost(distance, x) for x in crab_fuel])
+        if current < best_fuel:
+            best_fuel = current
+            best_point = distance
+
+    print(f"Part 2 score is {best_fuel}")
 if __name__ == "__main__":
 
     run()

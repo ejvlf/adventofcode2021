f1c44c7ac341e14e865bafd49b1244287cad0918
Emanuel Ferreira <ejvlf@outlook.pt>
2021-12-19 23:09:36 +0000

    day 17

---
+++ day18.py
@@ -0,0 +1,15 @@
+from common.text_manipulations import TextParser
+
+def run():
+
+    source = TextParser("day17.txt").load_file_as_raw_string()
+
+    # Part 1
+    print(f"Part 1 result: {}")
+
+    #Part 2
+    print(f"Part 2 result: {}")
+
+if __name__ == "__main__":
+
+    run()
